aab58b3143b278c433305717e8632c2be90a9b0b
Kazey100 <cyberleagecrmc9@gmail.com>
2024-12-16 16:37:28 +0800

    Initial commit

---
+++ src/main/java/com/SEG1/G1Application/G1Application.java
@@ -0,0 +1,13 @@
+package com.SEG1.G1Application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class G1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(G1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/SEG1/G1Application/G1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SEG1.G1Application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class G1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
