ad763b7b39df8a19dee9fd8bdcfcc6d4c68290b9
PollyYong <pollyyong1254@gmail.com>
2024-12-25 13:25:48 +0800

    done register

--- front-end/src/register.js
+++ front-end/src/register.js
@@ -1,42 +1,49 @@
-import React from "react";
+import React, { useState }  from "react";
 import { useNavigate } from "react-router-dom";
-
 import "./registerPage.css";
 
 function RegisterPage() {
-    const navigate = useNavigate();
+  const [isCollapsed, setIsCollapsed] = useState(false);
+  const navigate = useNavigate();
 
-    const handleButtonClick = () => {
-      navigate("/"); // Adjust the path to match the route in App.js
-    };
+  const handleButtonClick = () => {
+    navigate("/"); 
+  };
 
     return (
         <div className="registerPage">
-          {/* Upper*/}
+          {/*Upper*/}
           <div className="upper">
           <h1>NZ HOME</h1>
-        </div>
+          </div>
 
-         {/* Form*/}
+         {/*Form*/}
         <div className="form">
-          <img src="./image/NZHome.png" alt="NZ Home Logo"/>
+          <img src="./image/NZHome.png" alt="NZ Home Logo" className={`${isCollapsed ? "hidden" : "block"}`} />
           <h2>Register</h2>
 
-           <form>
+          <form>
             <input type="text" name="username" placeholder="Username" required/>
+            <br/>
+            <br/>
             <input type="email" name="email" placeholder="Email" required/>
+            <br/>
+            <br/>
             <input type="password" name="password" placeholder="Password" required/>
 
             <p>
-             <input type = "checkbox" name="checkbox" required /> I have read and agreed to the <p name="T&C"> Terms & Conditions of Use</p> 
+             <input type = "checkbox" name="checkbox" required /> I have read and agreed to the 
+             <a href =" " class="terms"> Terms & Conditions of Use</a> 
             </p>
 
-            <button type="submit" className="submitButton"> Sign Up </button>
+            <button type="submit" class="button1" onClick={handleButtonClick}> Sign Up </button>
 
-            <p>---------------- Or ----------------</p>
+            <p>-------------------------------- Or --------------------------------</p>
 
-            <button type="button" className="submitButton" onClick={handleButtonClick}>Log In</button>
-           </form>
+            <button type="button" class="button2" onClick={handleButtonClick}> Log In </button>
+            <br/>
+            <br/>
+          </form>
        </div>
        </div>
     );
