d7586009f354ec0ca6a34912d5876746e052963a
PollyYong <pollyyong1254@gmail.com>
2024-12-29 15:05:36 +0800

    Done profile base

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -6,9 +6,9 @@ import RoomsRobots from "./components/roomsRobots";
 import Devices from "./components/devices";
 import Users from "./components/users";
 
-import RoomPage from "./components/RoomPage";
-import DevicePage from "./components/DevicePage";
-import UserPage from "./components/UserPage";
+import RoomPage from "./components/pages/RoomPage";
+import DevicePage from "./components/pages/DevicePage";
+import UserPage from "./components/pages/UserPage";
 
 import "./App.css";
 
@@ -19,152 +19,149 @@ function App() {
   };
 
   return (
-    <Router>
-      <div className="bg-gray-800 font-sans leading-normal tracking-normal h-screen overflow-hidden">
-        <Routes>
-          {/* Routes that require a full layout */}
-          <Route
-            path="/"
-            element={
-              <div className="p-2 grid grid-cols-[auto_1fr] h-full">
-                <div className="relative flex">
-                  {/* Sidebar */}
-                  <div
-                    className={`sidebar ${
-                      isCollapsed ? "w-[0px]" : "w-[100px]"
-                    } ${
-                      isCollapsed ? "" : "bg-black"
-                    } rounded-lg min-h-full flex flex-col overflow-y-auto`}
-                  >
-                    {/* Sidebar Logo */}
-                    <div className="h-[100px] flex items-center justify-center">
-                      <a href="/">
-                        <img
-                          src="./image/NZHome.png"
-                          alt="NZ Home Logo"
-                          className={`${isCollapsed ? "hidden" : "block"}`}
-                        />
-                      </a>
-                    </div>
+    <Routes>
+      {/* Routes that require a full layout */}
+      <Route
+        path="/"
+        element={
+          <div className="bg-gray-800 p-2 grid grid-cols-[auto_1fr] h-full">
+            <div className="relative flex">
+              {/* Sidebar */}
+              <div
+                className={`sidebar ${isCollapsed ? "w-[0px]" : "w-[100px]"} ${
+                  isCollapsed ? "" : "bg-black"
+                } rounded-lg min-h-full flex flex-col overflow-y-auto`}
+              >
+                {/* Sidebar Logo */}
+                <div className="h-[100px] flex items-center justify-center">
+                  <a href="/">
+                    <img
+                      src="./image/NZHome.png"
+                      alt="NZ Home Logo"
+                      className={`${isCollapsed ? "hidden" : "block"}`}
+                    />
+                  </a>
+                </div>
 
-                    {/* Sidebar Items */}
-                    <a href="/">
-                      <div className="flex flex-col items-center justify-center px-4 py-2">
-                        <i
-                          className={`fas fa-layer-group text-white text-2xl ${
-                            isCollapsed ? "hidden" : "block"
-                          }`}
-                        ></i>
-                        {!isCollapsed && (
-                          <span className="text-white text-center text-sm mt-2">
-                            Devices
-                          </span>
-                        )}
-                      </div>
-                    </a>
-                    <a href="#">
-                      <div className="flex flex-col items-center justify-center px-4 py-2">
-                        <i
-                          className={`fas fa-chart-pie text-white text-2xl ${
-                            isCollapsed ? "hidden" : "block"
-                          }`}
-                        ></i>
-                        {!isCollapsed && (
-                          <span className="text-white text-center text-sm mt-2">
-                            Internet Usage
-                          </span>
-                        )}
-                      </div>
-                    </a>
-                    <a href="#">
-                      <div className="flex flex-col items-center justify-center px-4 py-2">
-                        <i
-                          className={`fas fa-wind text-white text-2xl ${
-                            isCollapsed ? "hidden" : "block"
-                          }`}
-                        ></i>
-                        {!isCollapsed && (
-                          <span className="text-white text-center text-sm mt-2">
-                            Calendar
-                          </span>
-                        )}
-                      </div>
-                    </a>
+                {/* Sidebar Items */}
+                <a href="/">
+                  <div className="flex flex-col items-center justify-center px-4 py-2">
+                    <i
+                      className={`fas fa-layer-group text-white text-2xl ${
+                        isCollapsed ? "hidden" : "block"
+                      }`}
+                    ></i>
+                    {!isCollapsed && (
+                      <span className="text-white text-center text-sm mt-2">
+                        Devices
+                      </span>
+                    )}
                   </div>
+                </a>
+                <a href="#">
+                  <div className="flex flex-col items-center justify-center px-4 py-2">
+                    <i
+                      className={`fas fa-chart-pie text-white text-2xl ${
+                        isCollapsed ? "hidden" : "block"
+                      }`}
+                    ></i>
+                    {!isCollapsed && (
+                      <span className="text-white text-center text-sm mt-2">
+                        Internet Usage
+                      </span>
+                    )}
+                  </div>
+                </a>
+                <a href="#">
+                  <div className="flex flex-col items-center justify-center px-4 py-2">
+                    <i
+                      className={`fas fa-wind text-white text-2xl ${
+                        isCollapsed ? "hidden" : "block"
+                      }`}
+                    ></i>
+                    {!isCollapsed && (
+                      <span className="text-white text-center text-sm mt-2">
+                        Calendar
+                      </span>
+                    )}
+                  </div>
+                </a>
+              </div>
 
-                  {/* Collapse Button */}
-                  <div
-                    className={`absolute top-1/2 transform -translate-y-1/2 transition-all duration-500 ease-in-out ${
-                      isCollapsed ? "left-[0px]" : "left-[80px]"
+              {/* Collapse Button */}
+              <div
+                className={`absolute top-1/2 transform -translate-y-1/2 transition-all duration-500 ease-in-out ${
+                  isCollapsed ? "left-[0px]" : "left-[80px]"
+                }`}
+              >
+                <button
+                  onClick={toggleSidebar}
+                  className={`text-white text-2xl bg-black p-2 rounded-full shadow-md transform transition-all duration-500 ease-in-out ${
+                    isCollapsed ? "scale-0 opacity-0" : "scale-100 opacity-100"
+                  }`}
+                >
+                  <i
+                    className={`fas ${
+                      isCollapsed ? "fa-chevron-left" : "fa-chevron-left"
                     }`}
-                  >
-                    <button
-                      onClick={toggleSidebar}
-                      className={`text-white text-2xl bg-black p-2 rounded-full shadow-md transform transition-all duration-500 ease-in-out ${
-                        isCollapsed
-                          ? "scale-0 opacity-0"
-                          : "scale-100 opacity-100"
+                  ></i>
+                </button>
+              </div>
+            </div>
+
+            {/* Main Content */}
+            <div className="main-content flex flex-col flex-1 transition-all duration-300 overflow-y-auto">
+              <div className="px-4 grid grid-rows-[5rem_1fr] flex-1">
+                {/* Header */}
+                <div className="flex justify-between items-center relative">
+                  <div className="bg-black rounded-lg w-full flex items-center px-4 py-4">
+                    {/* Hamburger Button */}
+                    <div
+                      className={`flex items-center ${
+                        isCollapsed ? "block" : "hidden"
                       }`}
                     >
-                      <i
-                        className={`fas ${
-                          isCollapsed ? "fa-chevron-left" : "fa-chevron-left"
-                        }`}
-                      ></i>
-                    </button>
-                  </div>
-                </div>
-
-                {/* Main Content */}
-                <div className="main-content flex flex-col flex-1 transition-all duration-300 overflow-y-auto">
-                  <div className="px-4 grid grid-rows-[5rem_1fr] flex-1">
-                    {/* Header */}
-                    <div className="flex justify-between items-center relative">
-                      <div className="bg-black rounded-lg w-full flex items-center px-4 py-4">
-                        {/* Hamburger Button */}
-                        <div
-                          className={`flex items-center ${
-                            isCollapsed ? "block" : "hidden"
-                          }`}
-                        >
-                          <button
-                            onClick={toggleSidebar}
-                            className="text-white text-2xl bg-transparent border-0 mr-4"
-                          >
-                            <i className="fas fa-bars"></i>
-                          </button>
-                        </div>
-
-                        {/* Centered Text */}
-                        <h1 className="font-bold text-white flex-grow text-center text-4xl">
-                          NZ HOME
-                        </h1>
-
-                        {/* Bell Icon */}
-                        <a href="#" className="ml-auto">
-                          <i className="fas fa-bell text-white text-3xl"></i>
-                        </a>
-                      </div>
+                      <button
+                        onClick={toggleSidebar}
+                        className="text-white text-2xl bg-transparent border-0 ml-4"
+                      >
+                        <i className="fas fa-bars"></i>
+                      </button>
                     </div>
 
-                    {/* Main Widgets */}
-                    <WidgetsEnergy />
-                    <RoomsRobots />
-                    <Devices />
-                    <Users />
+                    {/* Centered Text */}
+                    <h1 className="font-bold text-white flex-grow text-center text-4xl ml-[4%]">
+                      NZ HOME
+                    </h1>
+
+                    {/* User Icon */}
+                    <a href="#" className="mr-8">
+                      <i className="fas fa-user text-white text-3xl"></i>
+                    </a>
+
+                    {/* Bell Icon */}
+                    <a href="#" className="mr-5">
+                      <i className="fas fa-bell text-white text-3xl"></i>
+                    </a>
                   </div>
                 </div>
-              </div>
-            }
-          />
 
-          {/* Routes that display a blank page */}
-          <Route path="/room/:name" element={<RoomPage />} />
-          <Route path="/device/:name" element={<DevicePage />} />
-          <Route path="/user/:name" element={<UserPage />} />
-        </Routes>
-      </div>
-    </Router>
+                {/* Main Widgets */}
+                <WidgetsEnergy />
+                <RoomsRobots />
+                <Devices />
+                <Users />
+              </div>
+            </div>
+          </div>
+        }
+      />
+
+      {/* Routes that display a blank page */}
+      <Route path="/room/:name" element={<RoomPage />} />
+      <Route path="/device/:name" element={<DevicePage />} />
+      <Route path="/user/:name" element={<UserPage />} />
+    </Routes>
   );
 }
 
